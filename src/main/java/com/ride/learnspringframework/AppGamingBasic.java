1f8cc7640e8dc8206903564de0fc6c49180b6d21
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-11-25 23:53:10 +0900

    느슨한 결합 - interface 도입

--- src/main/java/com/ride/learnspringframework/AppGamingBasic.java
+++ src/main/java/com/ride/learnspringframework/AppGamingBasic.java
@@ -2,13 +2,15 @@ package com.ride.learnspringframework;
 
 import com.ride.learnspringframework.game.GameRunner;
 import com.ride.learnspringframework.game.MarioGame;
+import com.ride.learnspringframework.game.PacmanGame;
 import com.ride.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasic {
     public static void main(String[] args) {
-        var marioGame = new MarioGame();
-        var superContraGame = new SuperContraGame();
-        var gameRunner = new GameRunner(marioGame);
+//        var game = new MarioGame();
+//        var game = new SuperContraGame();
+        var game = new PacmanGame();
+        var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
 }

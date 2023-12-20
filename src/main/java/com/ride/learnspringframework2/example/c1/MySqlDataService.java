f07593e3e04bd09b7844aa17ff5b15bc8fc56009
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-12-20 16:04:19 +0900

    @Component 특수화(@Service, @Repository)

--- src/main/java/com/ride/learnspringframework2/example/c1/MySqlDataService.java
+++ src/main/java/com/ride/learnspringframework2/example/c1/MySqlDataService.java
@@ -1,8 +1,10 @@
 package com.ride.learnspringframework2.example.c1;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository
 public class MySqlDataService implements DataService{
     @Override
     public int[] retrieveData() {
